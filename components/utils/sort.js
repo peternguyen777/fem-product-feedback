ea66babdfe453fdd7622a9d1e596504cbebc1907
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-08-03 19:21:16 +1000

    sorting function

--- components/utils/sort.js
+++ components/utils/sort.js
@@ -2,30 +2,30 @@ export const sortRequests = (input, filterSelect, categorySelect) => {
   var categorizedInputs = input?.slice();
 
   if (categorySelect != "all") {
-    categorizedInputs = input.filter(
+    categorizedInputs = input?.filter(
       (item) => item.category.toLowerCase() === categorySelect.toLowerCase()
     );
   }
 
   if (filterSelect === "Most Upvotes") {
-    const result = categorizedInputs.sort((a, b) => b.upvotes - a.upvotes);
+    const result = categorizedInputs?.sort((a, b) => b.upvotes - a.upvotes);
     return result;
   }
 
   if (filterSelect === "Least Upvotes") {
-    const result = categorizedInputs.sort((a, b) => a.upvotes - b.upvotes);
+    const result = categorizedInputs?.sort((a, b) => a.upvotes - b.upvotes);
     return result;
   }
 
   if (filterSelect === "Most Comments") {
-    const result = categorizedInputs.sort(
+    const result = categorizedInputs?.sort(
       (a, b) => b.commentsList?.length - a.commentsList?.length
     );
     return result;
   }
 
   if (filterSelect === "Least Comments") {
-    const result = categorizedInputs.sort(
+    const result = categorizedInputs?.sort(
       (a, b) => a.commentsList?.length - b.commentsList?.length
     );
     return result;
